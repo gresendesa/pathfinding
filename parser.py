b88fd78d87f4f27198cb95d06278ad1da77eb097
gresendesa <gresendesa@gmail.com>
2019-06-30 14:38:21 -0300

    parser

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
 import pydot
-from pprint import pprint
 
-graph = pydot.graph_from_dot_file("g1.dot")
+class DOT:
 
-edges = graph[0].obj_dict["edges"]
-
-pprint(edges)
+	def __init__(self, filename):
+		graph = pydot.graph_from_dot_file(filename)
+		edges = graph[0].obj_dict["edges"]
+		self.vertices = list(set([edge[0] for edge in edges] + [edge[1] for edge in edges]))
+		self.edges = [{"edge": edge, "label":edges[edge][0]['attributes']['label']} for edge in edges]
