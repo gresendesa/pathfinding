dec1330bae792480b0e37880dd6e2b1279181082
gresendesa <gresendesa@gmail.com>
2019-07-01 01:29:08 -0300

    alg

--- algoritmos.py
+++ algoritmos.py
@@ -4,44 +4,40 @@ from pprint import pprint
 from grafos import Grafo
 
 #Explicação: https://www.youtube.com/watch?v=aJ_2c9NVCIc&t=15s
-def dijkstra(dot, vertice_inicial, vertice_destino):
-	tabela = {int(i) : {'weight':sys.maxint, 'origem':None} for i in dot.grafo.vs.indices} #Inicia a tabela com infinito
-	vertices_nao_visitados = dot.grafo.vs.indices #Inicia a lista de vértices não visitados
+def dijkstra(grafo, vertice_inicial, vertice_destino):
+	tabela = {int(i) : {'peso':sys.maxint, 'origem':None} for i in grafo.grafo.vs.indices} #Inicia a tabela com infinito
+	vertices_nao_visitados = grafo.grafo.vs.indices #Inicia a lista de vértices não visitados
 	vertices_visitados = []
 	vertice_sob_analise = vertice_inicial #Primeiro vértice sob análise é o vértice inicial
-	tabela.update({vertice_inicial: {'weight':0, 'origem':vertice_inicial}}) #Inicia a tabela do primeiro vértice com 0
+	tabela.update({vertice_inicial: {'peso':0, 'origem':vertice_inicial}}) #Inicia a tabela do primeiro vértice com 0
 
 	while len(vertices_nao_visitados) > 0: #O loop vai continuar até que o processo tenha sido finalizado
 
-		#print "__ Nó sob análise: %d" % vertice_sob_analise
-		#print "Nó %d adicionado à lista de visitados e removido da lista de não visitados" % vertice_sob_analise
 		vertices_visitados.append(vertice_sob_analise)
 		vertices_nao_visitados.remove(vertice_sob_analise)
-		#print "Vertices visitados %s" % ', '.join(str(e) for e in vertices_visitados)
-		#print "Vertices não visitados %s" % ', '.join(str(e) for e in vertices_nao_visitados)
 
 		for vertice in vertices_nao_visitados:
 			try:
-				weight = dot.grafo.es.find(_within=[vertice_sob_analise,vertice])['weight']
-				#print "Aresta (%d %d) %d < %f?" % (vertice_sob_analise, vertice,tabela[vertice_sob_analise]['weight'] + weight,tabela[vertice]['weight'])
-				if tabela[vertice_sob_analise]['weight'] + weight < tabela[vertice]['weight']:
-					tabela.update({vertice: {'weight': tabela[vertice_sob_analise]['weight'] + weight, 'origem': vertice_sob_analise}})
+				peso = grafo.grafo.es.find(_within=[vertice_sob_analise,vertice])['weight']
+				if tabela[vertice_sob_analise]['peso'] + peso < tabela[vertice]['peso']:
+					tabela.update({vertice: {'peso': tabela[vertice_sob_analise]['peso'] + peso, 'origem': vertice_sob_analise}})
 			except ValueError:
-				#print "Aresta (%d %d) não encontrada" % (vertice_sob_analise, vertice)
 				pass
 
-
-		#print tabela
 		#Escolhe o próximo vértice sob análise
 		min = sys.maxint
 		for vertice in vertices_nao_visitados:
-			if tabela[vertice]['weight'] <= min:
-				min = tabela[vertice]['weight']
+			if tabela[vertice]['peso'] <= min:
+				min = tabela[vertice]['peso']
 				vertice_sob_analise = vertice
-		#print "Próximo vértice escolhido: %d" % vertice_sob_analise
+
 	pprint(tabela)
 
 def bellman_ford():
+	tabela = {int(i) : {'peso':sys.maxint, 'origem':None} for i in grafo.grafo.vs.indices} #Inicia a tabela com infinito
+	tabela.update({vertice_inicial: {'peso':0, 'origem':vertice_inicial}}) #Inicia a tabela do primeiro vértice com 0
+
+	peso = grafo.grafo.es.find(_from=vertice_sob_analise,_to=vertice)['peso']
 	pass
 
 def rpf():
@@ -50,6 +46,6 @@ def rpf():
 def spanning_tree():
 	pass
 
-dot = Grafo("g1.dot")
-msg = lambda s : "Nó de %s [%s] " % (s,'|'.join(str(e) for e in dot.vertices))
-dijkstra(dot=dot,vertice_inicial=int(input(msg('origem'))),vertice_destino=int(input(msg('destino'))))
+grafo = Grafo("g1.dot")
+msg = lambda s : "Nó de %s [%s] " % (s,'|'.join(str(e) for e in grafo.vertices))
+dijkstra(grafo=grafo,vertice_inicial=int(input(msg('origem'))),vertice_destino=int(input(msg('destino'))))
